09a6fcd1c259f43de8361390c61dde65a52c3dbd
unknown <Nakov@Nakov-PC.(none)>
2012-04-29 11:19:51 +0300

    Initially loaded the project in Git

---
+++ CinExample.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Nakov.IO; // see http://www.nakov.com/tags/cin
+
+public class CinExample
+{
+	static void Main()
+	{
+		Console.Write("Enter your name: ");
+		string name = Console.ReadLine();
+
+		Console.Write("Enter two integers x and y separated by whitespace: ");
+		// cin >> x >> y;
+		int x = Cin.NextInt();
+		double y = Cin.NextDouble();
+
+		Console.Write("Enter your age: ");
+		int age = int.Parse(Console.ReadLine());
+
+		Console.WriteLine("Name: {0}, Age: {1}", name, age);
+		Console.WriteLine("x={0}, y={1}", x, y);
+
+		Console.Write("Enter a positive integer number N: ");
+		int n = Cin.NextInt();
+
+		Console.Write("Enter N decimal numbers separated by a space: ");
+		decimal[] numbers = new decimal[n];
+		for (int i = 0; i < n; i++)
+		{
+			numbers[i] = Cin.NextDecimal();
+		}
+
+		Console.Write("The numbers in ascending order: ");
+		Array.Sort(numbers);
+		for (int i = 0; i < n; i++)
+		{
+			Console.Write(numbers[i]);
+			Console.Write(' ');
+		}
+		Console.WriteLine();
+
+		Console.WriteLine("Enter two strings seperated by a space: ");
+		string firstStr = Cin.NextToken();
+		string secondStr = Cin.NextToken();
+		Console.WriteLine("First str={0}", firstStr);
+		Console.WriteLine("Second str={0}", secondStr);
+	}
+}
--- README.md
+++ README.md
@@ -2,3 +2,5 @@ nakov.io.cin
 ============
 
 Nakov.IO.CIN - Console input for C# in the C++ cin style
+
+See more at http://www.nakov.com/tags/cin
